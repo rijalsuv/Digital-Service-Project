0e0f3a7bf5027159dc7ba4135ec4f4331b01dbe1
rijalsuv <rijalsuv@gmail.com>
2020-03-21 11:47:44 +0200

    Digital Service Project

---
+++ JavaScript.js
@@ -0,0 +1,17 @@
+﻿/*jslint browser:true*/
+/*global console*/
+/*global window*/
+/* The lines above are for the www.jslint.com JavaScript "validator" */
+/* Just keep them like they are. Also start your functions with "use strict"; */
+/* And remove extra spaces after every code or comment line. */
+
+// A. Creating/defining a function. Note the body within curly brackets {  }
+function helloWorld() {
+    "use strict";
+
+    console.log("Hello World!");
+    // Note: NOT printing to the body = canvas BUT to the developer console
+}
+
+// B. Calling the function created above. Note semicolon at the end.
+helloWorld();
